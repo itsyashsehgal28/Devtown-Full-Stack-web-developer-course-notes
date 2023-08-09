ab78d823c7a927b4fd2cf59bf7ef17a04595267e
itsyashsehgal28 <yashsehgal2000@gmail.com>
2023-08-09 12:00:44 +0530

    files organized

---
+++ 5. React.JS/day 48 intro to react.js
@@ -0,0 +1,6 @@
+// string is a primitive datatype because strings are immutable , means they can't be changed once initialized that's why they are 
+// primitive 
+
+// string str = "yash" =====> str = "shagun" ===> this will be stored in a new location rather than the same location 
+// it may appear that they are the same string but they are not 
+
