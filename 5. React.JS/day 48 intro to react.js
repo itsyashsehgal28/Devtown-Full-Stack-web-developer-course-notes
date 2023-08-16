5e47c15234dde619955973ee268d514e8f3dfab7
itsyashsehgal28 <yashsehgal2000@gmail.com>
2023-08-16 13:46:22 +0530

    updated

--- 5. React.JS/day 48 intro to react.js
+++ 5. React.JS/day 48 intro to react.js
@@ -1,6 +1,38 @@
-// string is a primitive datatype because strings are immutable , means they can't be changed once initialized that's why they are 
-// primitive 
+// React Js can have 2 extensions :    .js(javascript)  &&  .ts(typescript)
 
-// string str = "yash" =====> str = "shagun" ===> this will be stored in a new location rather than the same location 
-// it may appear that they are the same string but they are not 
+// also instead of js we can use .jsx extension (Javascript XML file)  ---> combo of Javascript + Html file
 
+// since we have done a lot of projects in js we will be using and learning TYPESCRIPT now for React 
+
+// TYPESCRIPT : not much different than js , its ES6 (ecma script es6) script which is built on JS6 version of Javascript
+
+/* 
+
+    SPA - Single Page Application ---> on click of any link or button the page url should not change 
+
+    Since everything is present in 1 page or 1 file it should take a lot of time to load but react or angular or vue js
+    dont let us waste this time by dividing the page into components , different components , it will only provide 
+    that component which is needed at that time or rendering only those things that are needed 
+
+    Eg: Gmail , Instagram
+*/
+
+
+/*
+    Things we have in React : 
+    --->    Components   : Group Codes together to form components
+
+    --->    Data Binding : Binding Data With respect to your component 
+                           Uni-Directional Flow of Data
+
+                           Uni-Directional means the flow of data is from parent element to child element not vice-versa
+                           Eg: home.jsx ---> navbar.jsx ----> aboutus.jsx 
+                           We Cannot Go the other way hence uni directional flow of data
+
+    --->    Virtual DOM  : In DOM operations in JS if we change an element whole page gets refreshed but in virtual DOM
+                           only the element gets refreshed , no need to load the whole page 
+
+                           Eg: updated <p> in <body> ----> Virtual DOM ----> DOM ----> Html manipulations reflected on page
+
+    --->    
+*/
